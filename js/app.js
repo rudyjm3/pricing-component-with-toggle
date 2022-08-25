eff34126937b0054802d2547425b1101904a4cd1
rudyjm3 <rudyjm3@yahoo.com>
2022-08-24 23:30:45 -0400

    Fix styling issues with body, footer, font size, spacing.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 
 const checkbox = document.getElementById('check-box');
-console.log(check-box);
+console.log(checkbox);
+
+checkbox.addEventListener('click', () => {
+   if (condition) {
+      
+   } else {
+      
+   }
+})
+
+
+// function toggle() {
+//    if (checkbox.checked == checkbox.checked) {
+//       alert("toggle is checked");
+//    } else {
+//       alert("toggle is un checked");
+//    }
+   
+//    // if (checkbox != checkbox.checked) {
+//    //    alert("toggle is unchecked");
+//    // }
+
+// };
+// toggle();
