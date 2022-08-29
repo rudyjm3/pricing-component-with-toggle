a24e943c0cd7706466afa27d4f26ca51ecaae8b5
rudyjm3 <rudyjm3@yahoo.com>
2022-08-29 10:39:50 -0400

    Uopdated function to change price depending on toogle position. Updated gitignore file.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,28 @@
 
 const checkbox = document.getElementById('check-box');
-let prices = document.querySelectorAll('.price');
+const basicPrice = document.getElementsByClassName('price')[0];//Can not have .innerHTML, innerText, or textContent. Just the index[0] of class
+const proPrice = document.getElementsByClassName('price')[1];
+const masterPrice = document.getElementsByClassName('price')[2];
 
-console.log(checkbox);
-console.log(prices);
+console.log(basicPrice, proPrice, masterPrice);
 
 checkbox.addEventListener('change', (e) => {
 
-   if (e.target.checked) {
-      alert ("Toggle is in checked position"); 
-      console.log("Toggle is in checked position");
+   if (e.target.checked) { 
+      basicPrice.textContent = "19.99";
+      proPrice.textContent = "24.99";
+      masterPrice.textContent = "39.99";
    } else {
-      alert("Not checked");
-      console.log("Not checked");
+      basicPrice.innerText = "199.99";
+      proPrice.innerText = "249.99";
+      masterPrice.innerText = "399.99";
    }
 
 });
-
+// let x = 10;
+// function monthly(a, b) {
+//    return ((a * 100) / b).toFixed(2);
+// }
+// let result = monthly(basicPrice, x);
+// console.log(basicPrice);
+// console.log(result);
